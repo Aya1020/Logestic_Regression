1f650a165b927c134fb3d0fa2dae0c9b507142bf
Aya Muhammed Abdelghany <102863563+Aya1020@users.noreply.github.com>
2022-11-22 21:42:50 +0200

    Update

--- Logestic_Regression DM Patients.R
+++ Logestic_Regression DM Patients.R
@@ -1,21 +1,65 @@
 library(tidyverse)
 #Importing the data
-#he “header=TRUE” option tells R that the first row in the file contains the column names.
-dt = diapetic_patients <- read_csv("Logestic_Regression/Data/diapetic patients.csv")
+dt = read_csv("Data/diapetic patients.csv")
 view(dt)
-#To inspect the dimentions of my data 
+#To inspect the dimensions of my data 
 dim(dt) # So i have 403 rows, in this case patients, and 24 columns
 
 #tell R which variables are categorical, as it will assume they’re all continuous by default:
 chol = dt["chol"] # cholesterol is continuous, so it’s easy
-gender = as.factor(dt[,"gender"]) # but gender isn’t.
-dm = as.factor(dt[,"dm"]) # neither is dm
+gender = as.factor(dt$gender) # but gender isn’t.
+dm = as.factor(dt$dm)
 
-#To see how many males and females & getting the total, useing the “table” command.
+#To see how many males and females & getting the total, using the “table” command.
 t <- table(gender) # store the tabulation for further manipulation
-addmargins(t) # this will sum up the gender totals to give an overall total and print the results
+addmargins(t)  # this will sum up the gender totals to give an overall total and print the results
 
 
 #Getting percentages of the categories
 round(prop.table(t),digits=3) # get proportions rounded to 3dp
 round(100*prop.table(t),digits=1) # get %s rounded to 1dp.
+
+is.na(dt$dm) %>% sum() #to know the count of the missing data
+dm2 = factor(dm, exclude=NULL) # make new factor from the old one
+table(dm2) # display the counts including the missings (NAs)
+
+summary(chol)
+hist(dt$chol,
+     main = "Histogram of Cholesterol",
+     col = "Dark RED",
+     breaks = 10)
+
+height = dt$height
+weight = dt$weight
+summary(height)
+summary(weight)
+
+height.si = height*0.0254
+weight.si = weight*0.453592
+bmi = weight.si/height.si^2
+
+summary(bmi)
+hist(bmi,
+     main = "Histogram of BMI",
+     col = "Dark BLUE",
+     breaks = 6)
+#If your BMI is less than 18.5, it falls within the underweight range.
+#If your BMI is 18.5 to 24.9, it falls within the normal or Healthy Weight range.
+#If your BMI is 25.0 to 29.9, it falls within the overweight range.
+#If your BMI is 30.0 or higher, it falls within the obese range.
+
+bmi_categorised <- ifelse(bmi < 18.5, "underweight", 
+                          ifelse(bmi >= 18.5 & bmi <= 25, "normal", 
+                                 ifelse(bmi > 25 & bmi <= 30, "overweight", 
+                                        ifelse(bmi > 30, "obese", NA)))) 
+
+# check that the bmi_categorised variable has worked  
+table(bmi_categorised, exclude = NULL) 
+
+# frequencies of diabetes by BMI category 
+dm_by_bmi_category <- table(bmi_categorised, dm2, exclude = NULL) 
+
+# check 
+dm_by_bmi_category 
+
+round(100 * prop.table(dm_by_bmi_category, margin = 1), digits = 1) 
